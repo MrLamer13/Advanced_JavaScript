b0ae028d54461354b0ef94a5c9e46ceca9d01484
MrLamer <mrlamer@mail.ru>
2022-09-23 13:27:28 +0700

    Практическое задание №2

---
+++ 01-02/js/main.js
@@ -0,0 +1,112 @@
+class ProductList {
+    constructor(container = '.products') {
+        this.container = container;
+        this.goods = [];
+        this._fetchProducts();//рекомендация, чтобы метод был вызван в текущем классе
+        this.render();//вывод товаров на страницу
+    }
+
+
+
+    _fetchProducts() {
+        this.goods = [
+            { id: 1, title: 'Notebook', price: 2000 },
+            { id: 2, title: 'Mouse', price: 20 },
+            { id: 3, title: 'Keyboard', price: 200 },
+            { id: 4, title: 'Gamepad', price: 50 },
+        ];
+    }
+
+    render() {
+        const block = document.querySelector(this.container);
+        for (let product of this.goods) {
+            const item = new ProductItem(product);
+            block.insertAdjacentHTML("beforeend", item.render());
+            //              block.innerHTML += item.render();
+        }
+    }
+
+    totalPrice() {
+        let result = 0;
+        for (let value of this.goods) {
+            result += value.price;
+        }
+        return result;
+    }
+}
+
+class ProductItem {
+    constructor(product, img = 'https://via.placeholder.com/200x150') {
+        this.title = product.title;
+        this.id = product.id;
+        this.price = product.price;
+        this.img = img;
+    }
+    render() {
+        return `<div class="product-item">
+                <img src="${this.img}">
+                <h3>${this.title}</h3>
+                <p>${this.price}</p>
+                <button class="buy-btn">Купить</button>
+            </div>`
+    }
+}
+
+let list = new ProductList();
+
+
+class ProductCart {
+    constructor() {
+        this.cart = [];
+        this.cartCount = 0;
+    }
+
+    addProductCart() {
+        item = new ProductCartItem();
+        this.cart += item;
+        this.incCartCount();
+    }
+
+    incCartCount() {
+        this.cartCount++;
+    }
+
+    deleteProductCart() {
+        this.decCartCount();
+    }
+
+    decCartCount() {
+        this.cartCount--;
+    }
+
+
+}
+
+class ProductCartItem {
+    constructor(id, title, price) {
+        this.id = id;
+        this.title = title;
+        this.price = price;
+    }
+}
+
+
+
+//const products = [
+//    {id: 1, title: 'Notebook', price: 2000},
+//    {id: 2, title: 'Mouse', price: 20},
+//    {id: 3, title: 'Keyboard', price: 200},
+//    {id: 4, title: 'Gamepad', price: 50},
+//];
+//
+//const renderProduct = (product,img='https://placehold.it/200x150') => {
+//    return `<div class="product-item">
+//                <img src="${img}">
+//                <h3>${product.title}</h3>
+//                <p>${product.price}</p>
+//                <button class="buy-btn">Купить</button>
+//            </div>`
+//};
+//const renderPage = list => document.querySelector('.products').innerHTML = list.map(item => renderProduct(item)).join('');
+//
+//renderPage(products);
